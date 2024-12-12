ba715d88945337db052555dd09f77a3b05b402a8
MattMilfont <matheusnmilfontc@gmail.com>
2024-12-12 11:04:20 -0300

    [Fixing] Adding the SDK 24 required

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sensor_audio/sound_sensor_main_page.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:sensor_audio/permission.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +11,98 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sensor de Som',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const AudioSensorScreen(),
+    return const MaterialApp(
+      home: AudioRecorder(),
     );
   }
 }
+
+class AudioRecorder extends StatefulWidget {
+  const AudioRecorder({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _AudioRecorderState createState() => _AudioRecorderState();
+}
+
+class _AudioRecorderState extends State<AudioRecorder> {
+  final FlutterSoundRecorder _recorder = FlutterSoundRecorder();
+  bool _isRecording = false;
+  double _volume = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeRecorder();
+    checkPermissions();
+  }
+
+  // Inicializa o gravador de áudio
+  Future<void> _initializeRecorder() async {
+    await _recorder.openRecorder();
+    _recorder.setSubscriptionDuration(const Duration(
+        milliseconds: 100)); // Ajuste o tempo de intervalo para o volume
+    _recorder.onProgress!.listen((e) {
+      // Monitorando o volume durante a gravação
+      if (e.decibels != null) {
+        setState(() {
+          _volume = e.decibels!;
+        });
+      }
+    });
+  }
+
+  // Inicia a gravação de áudio
+  Future<void> _startRecording() async {
+    await _recorder.startRecorder(toFile: 'audio.aac');
+    setState(() {
+      _isRecording = true;
+    });
+  }
+
+  // Para a gravação de áudio
+  Future<void> _stopRecording() async {
+    await _recorder.stopRecorder();
+    setState(() {
+      _isRecording = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    _recorder.closeRecorder();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sensor de Áudio em decibéis'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _isRecording ? 'Recording...' : 'Press to Start Recording',
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _isRecording ? _stopRecording : _startRecording,
+              child: Text(_isRecording ? 'Stop Recording' : 'Start Recording'),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Current Volume: ${_volume.toStringAsFixed(2)} dB',
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
--- lib/permission.dart
+++ lib/permission.dart
@@ -1,6 +1,6 @@
 import 'package:permission_handler/permission_handler.dart';
 
-Future<bool> requestMicrophonePermission() async {
-  var status = await Permission.microphone.request();
-  return status.isGranted;
+Future<void> checkPermissions() async {
+  await Permission.microphone.request();
+  await Permission.storage.request();
 }
